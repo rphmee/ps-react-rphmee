80fef6e867c7fcd34ffb25785055c076f4978653
Ryan Mee <rphmee@gmail.com>
2019-05-12 11:22:24 -0400

    Tests Started

---
+++ src/utils/percentUtils.js
@@ -0,0 +1,4 @@
+// Return the width of a percent of total width, as an integer
+export function getWidthAsPercentOfTotalWidth(percent, totalWidth) {
+  return parseInt(totalWidth * (percent / 100), 10);
+}
