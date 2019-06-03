0f8d03607342e01f6c7f12eaf83d5ec553a2bbf3
Ryan Mee <rphmee@gmail.com>
2019-06-02 21:49:34 -0400

    NPM Publishing Config

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+export { default as ClickToEnterTextField } from "./ClickToEnterTextField";
+export { default as Ellipses } from "./Ellipses";
+export { default as ExpandingBar } from "./ExpandingBar";
+export { default as EyeIcon } from "./EyeIcon";
+export { default as ForTesting } from "./ForTesting";
+export { default as HelloWorld } from "./HelloWorld";
+export { default as IxButton } from "./IxButton";
+export { default as IxButtonMainFlat } from "./IxButtonMainFlat";
+export { default as IxButtonMulti } from "./IxButtonMulti";
+export { default as Label } from "./Label";
+export { default as PasswordInput } from "./PasswordInput";
+export { default as ProgressBar } from "./ProgressBar";
+export { default as RegistrationForm } from "./RegistrationForm";
+export { default as TextInput } from "./TextInput";
+export { default as TextInputBEM } from "./TextInputBEM";
+export { default as TextInputCSSModules } from "./TextInputCSSModules";
+export {
+  default as TextInputStyledComponents
+} from "./TextInputStyledComponents";
+export { default as Tooltip } from "./Tooltip";
